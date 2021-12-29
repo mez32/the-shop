d9756d02a3943ada6f994cd11345c317f8caf613
Alex Mesmer <mezz@Alexs-MacBook-Air.local>
2021-12-29 00:06:06 -0500

    frontend and backend code added up to payment method screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,38 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+
 const App = () => {
 	return (
-		<>
-			<h1>Welcome to the shop</h1>
-		</>
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Routes>
+						<Route path='/' element={<HomeScreen />} />
+						<Route path='/product/:id' element={<ProductScreen />} />
+						<Route path='/cart/:id' element={<CartScreen />} />
+						<Route path='/cart/' element={<CartScreen />} />
+						<Route path='/login' element={<LoginScreen />} />
+						<Route path='/register' element={<RegisterScreen />} />
+						<Route path='/profile' element={<ProfileScreen />} />
+						<Route path='/shipping' element={<ShippingScreen />} />
+						<Route path='/payment' element={<PaymentScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
 	)
 }
 
